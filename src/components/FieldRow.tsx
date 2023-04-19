e19c4dc0a7ba406fabafb387e194f5f528718826
Pranav Yadav <Pranavyadav3912@gmail.com>
2023-04-19 22:21:56 +0530

    refactor: use proxy name state and debounce changes

--- src/components/FieldRow.tsx
+++ src/components/FieldRow.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FieldTypes, Field } from "../types";
 import { MdAdd, MdDeleteOutline } from "react-icons/md";
 
@@ -6,29 +6,28 @@ export default function FieldRow({
   field,
   idx,
   onAddField,
+  onNameChange,
   onTypeChange,
   onRequiredChange,
 }: FieldRowProps): JSX.Element {
-  const handleClickName = (
-    evt: React.MouseEvent<HTMLParagraphElement>,
-    id: string
-  ) => {
-    const elemId = evt.currentTarget.id;
-    const input = document.getElementById(id);
-    if (input) {
-      input.focus();
-      input.contentEditable = "true";
-      input.addEventListener("keydown", (evt) => {
-        if (evt.key === "Enter") {
-          evt.preventDefault();
-          input.contentEditable = "false";
-        }
-      });
-      input.addEventListener("blur", () => {
-        input.contentEditable = "false";
-      });
-    }
-  };
+  const [fieldName, setFieldName] = React.useState<string>("" + field.name);
+  const handleNameChange = React.useMemo(
+    () => (evt: React.ChangeEvent<HTMLInputElement>) => {
+      const newName = evt.target.value;
+      if (field.name === newName) return; // avoid unnecessary re-renders
+      setFieldName(newName);
+    },
+    []
+  );
+
+  // debounce name change from input name poxy
+  useEffect(() => {
+    if (field.name === fieldName) return; // avoid unnecessary re-renders
+    const timeoutId = setTimeout(() => {
+      onNameChange(field.id, idx, fieldName);
+    }, 250);
+    return () => clearTimeout(timeoutId);
+  }, [fieldName]);
 
   const handleClickDelete = (id: string, idx: number) => {
     // TODO: update state
@@ -38,21 +37,22 @@ export default function FieldRow({
   return (
     <div className="flex flex-row justify-between items-center my-2 ml-4 py-1 border-b text-black dark:text-white hover:bg-gray-100 dark:hover:bg-neutral-700">
       <div className="flex flex-row">
+        {/* Input for field name, controlled using proxy name state */}
         <div className="flex flex-col rounded-lg">
-          <p
-            className="text px-3 rounded-lg hover:bg-white hover:text-black focus:bg-white focus:text-black"
+          <input
+            type="text"
+            className="text px-3 rounded-lg hover:bg-white hover:text-black focus:bg-white focus:text-black focus:scale-105 focus:shadow-lg focus:outline-neutral dark:focus:outline-gray-200 dark:focus:border dark:focus:border-gray-200 overflow-x"
             id={field.id}
-            onClick={(evt) => {
-              evt.preventDefault();
-              handleClickName(evt, field.id);
-            }}
             placeholder="addName"
             autoCorrect="false"
             spellCheck="false"
             title="Click to edit"
-          >
-            {field.name || "addName"}
-          </p>
+            onChange={handleNameChange}
+            minLength={2}
+            maxLength={20}
+            size={fieldName.length}
+            value={fieldName}
+          ></input>
         </div>
         {/* dropdown to select type prop*/}
         <div className="flex flex-col px-2 ml-2">
@@ -61,11 +61,15 @@ export default function FieldRow({
             id="type"
             defaultValue={field.type}
             onChange={(evt) => {
-              evt.preventDefault();
-              onTypeChange(field.id, idx, evt.currentTarget.value);
+              onTypeChange(
+                field.id,
+                idx,
+                field.type,
+                evt.target.value as FieldTypes
+              );
             }}
             title="Click to select"
-            className="rounded-lg cursor-pointer bg-gray-100 dark:bg-neutral-700 hover:bg-white dark:hover:bg-neutral-700"
+            className="rounded-lg cursor-pointer bg-gray-100 dark:bg-neutral-700 hover:bg-white dark:hover:bg-neutral-700 focus:outline-gray-200"
           >
             {Object.values(FieldTypes).map((val) => (
               <option key={val} value={val}>
@@ -78,16 +82,16 @@ export default function FieldRow({
       <div className="flex flex-row">
         {/* switch for required prop */}
         <div className="flex flex-row items-center ml-2">
-          <label htmlFor="required" className="mr-2 cursor-pointer">
+          <label htmlFor={`required-${field.id}`} className="mr-2">
             Required
           </label>
           <input
+            id={`required-${field.id}`}
             type="checkbox"
             checked={field.required}
             className="cursor-pointer"
             onChange={(evt) => {
-              evt.preventDefault();
-              onRequiredChange(field.id, idx, evt.currentTarget.checked);
+              onRequiredChange(field.id, idx, evt.target.checked);
             }}
           />
         </div>
@@ -95,9 +99,8 @@ export default function FieldRow({
         {field.type === FieldTypes.OBJECT && (
           <button
             className="hover:bg-violet-500 p-2 dark:hover:bg-violet-500 font-extrabold rounded-xl ml-2"
-            onClick={(evt) => {
-              evt.preventDefault();
-              onAddField(field.id, idx);
+            onClick={(evt: React.MouseEvent<HTMLButtonElement>) => {
+              onAddField(field.id);
             }}
             title="Click to add new field"
           >
@@ -108,7 +111,6 @@ export default function FieldRow({
         <button
           className="hover:bg-red-400 p-2 dark:hover:bg-red-500 font-bold rounded-xl ml-2"
           onClick={(evt) => {
-            evt.preventDefault();
             handleClickDelete(field.id, idx);
           }}
           title="Click to delete this field"
@@ -123,7 +125,13 @@ export default function FieldRow({
 export type FieldRowProps = {
   field: Field;
   idx: number;
-  onAddField: (id: string, idx: number) => void;
-  onTypeChange: (id: string, idx: number, type: string) => void;
+  onAddField: (id: string) => void;
+  onNameChange: (id: string, idx: number, name: string) => void;
+  onTypeChange: (
+    id: string,
+    idx: number,
+    currentType: FieldTypes,
+    type: FieldTypes
+  ) => void;
   onRequiredChange: (id: string, idx: number, required: boolean) => void;
 };
